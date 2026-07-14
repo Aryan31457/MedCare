8dfcf31be4748ba0096047a5777453e64458db84
Aryan@31457 <aryan31457@gmail.com>
2026-07-14 21:41:31 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -813,4 +813,4 @@ if os.path.exists(FRONTEND_DIST_DIR):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", reload=True)
