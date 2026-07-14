e5f0ca701d45b4cc8cd3a485783cea3ab28bbfb5
Aryan@31457 <aryan31457@gmail.com>
2026-07-14 21:39:13 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://medcare-lvto.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
